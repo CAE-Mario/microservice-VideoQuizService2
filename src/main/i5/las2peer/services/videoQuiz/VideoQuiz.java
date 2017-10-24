e4252ec81ae25fe7a9ab91d2d1ce197f3cff07f9
CAEmario <rosenst@dbis.rwth-aachen.de>
2017-10-24 21:57:18 +0200

    Http Response[] edited by Mario Rosenstengel

--- src/main/i5/las2peer/services/videoQuiz/VideoQuiz.java
+++ src/main/i5/las2peer/services/videoQuiz/VideoQuiz.java
@@ -107,8 +107,7 @@ public class VideoQuiz extends RESTService {
   })
   @ApiOperation(value = "GetQuiz", notes = " ")
   public Response GetQuiz(@PathParam("id") String id) {
- 
-    // 
+    //  
     boolean _condition = true;
     if(_condition) {
       JSONObject  = new JSONObject();
